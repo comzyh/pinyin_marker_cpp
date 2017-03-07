6b5c0cb390c134952b8240d25cfdfc228dd1890d
Comzyh <comzyh@gmail.com>
2017-03-07 18:25:22 +0800

    measure time.

--- test.cpp
+++ test.cpp
@@ -5,7 +5,12 @@
 #include <string>
 #include <cstdio>
 #include <vector>
+#include <chrono>
+
 using namespace std;
+using namespace std::chrono;
+using std::chrono::system_clock;
+
 const vector<string> dicts = {
   "clean_aizuyan_words.dat",
   "clean_janx_phrases.dat",
@@ -16,7 +21,6 @@ const vector<string> dicts = {
 };
 int main() {
 
-  std::ifstream dictfile("clean_janx_phrases.dat");
   std::string buffer;
   std::u32string line;
 
@@ -26,14 +30,27 @@ int main() {
   cout << "Building." << endl;
   marker.build();
   cout << "Build finish." << endl;
-  while (getline(cin, buffer)) {
+
+  std::ifstream testfile("small");
+  system_clock::time_point start_time = system_clock::now();
+  while (getline(testfile, buffer)) {
     u32string line = pinyin_marker::converter.from_bytes(buffer);
+    size_t end = 0;
+    for (size_t i = 0; i < line.length(); i++) {
+      if (line[i] == ' '){
+        continue;
+      }
+      line[end++] = line[i];
+    }
+    line.resize(end);
     vector<string> result(line.length(), "");
     marker.mark(line, result);
-    for (auto it = result.begin(); it != result.end(); it ++) {
-      cout << *it << ", ";
-    }
-    cout << endl;
+    // for (auto it = result.begin(); it != result.end(); it ++) {
+    //   cout << *it << ", ";
+    // }
+    // cout << endl;
   }
+  auto finish_time = system_clock::now();
+  printf("Finsih Searching: %.4lf s\n", duration_cast<milliseconds>(finish_time - start_time).count()/ 1000.0);
   return 0;
 }
